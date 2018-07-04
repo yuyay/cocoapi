706a0aae1fc253e1b2abbfbec7c3396ded365166
Yuya Yoshikawa <yosshi71jp@gmail.com>
2018-07-04 20:11:07 +0900

    Set encoding when load json file

--- PythonAPI/pycocotools/coco.py
+++ PythonAPI/pycocotools/coco.py
@@ -81,7 +81,7 @@ class COCO:
         if not annotation_file == None:
             print('loading annotations into memory...')
             tic = time.time()
-            dataset = json.load(open(annotation_file, 'r'))
+            dataset = json.load(open(annotation_file, 'r', encoding='utf8'))
             assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
             print('Done (t={:0.2f}s)'.format(time.time()- tic))
             self.dataset = dataset
